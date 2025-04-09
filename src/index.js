547392650612075ee6a325fcb289c3022efee387
createlsworklappy <justinecastillo7@gmail.com>
2025-04-09 15:49:07 +0900

    Function: add appropriate selectors for nav buttons and implement page wipe behavior when clicked

--- src/index.js
+++ src/index.js
@@ -5,3 +5,17 @@ import genHomePage from './home';
 document.addEventListener("DOMContentLoaded", () => {
     genHomePage();
 });
+
+const content = document.querySelector("#content");
+const home = document.querySelector("#home");
+const menu = document.querySelector("#menu");
+const about = document.querySelector("#about");
+
+const buttons = [home, menu, about];
+
+function clearTab() {
+    content.innerHTML = "";
+}
+
+buttons.forEach(button => button.addEventListener("click", clearTab));
+
