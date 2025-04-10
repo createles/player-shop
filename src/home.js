c545edf4612620614f5cfba5104066dbfab5d5c6
createlsworklappy <justinecastillo7@gmail.com>
2025-04-10 15:57:31 +0900

    Func: set thumbprint behavior to reveal nav buttons and open the player shop

--- src/home.js
+++ src/home.js
@@ -1,5 +1,11 @@
 import ThumbImage from './images/thumbprint-3.svg';
 
+const home = document.querySelector("#home");
+const menu = document.querySelector("#menu");
+const about = document.querySelector("#about");
+
+const navButtons = [home, menu, about];
+
 function genHomePage() {
     const content = document.querySelector("#content");
 
@@ -30,6 +36,26 @@ function genHomePage() {
     const thumbScanner = document.createElement("img");
     thumbScanner.src = ThumbImage;
     thumbScanner.alt = "Thumbprint Scanner";
+    // Logic for click&hold behavior for Thumbprint Scanner
+    let holdTimer;
+    // Mousedown eventListener to trigger setTimeout()
+    thumbScanner.addEventListener("mousedown", () => {
+        holdTimer = setTimeout(() => {
+            console.log("Identity verified. Player shop is now available.");
+            // Nav buttons made visible after identification confirmed
+            navButtons.forEach(button => button.style.background = "none");
+            navButtons.forEach(button => button.style.color = "rgba(255, 255, 255)");
+        }, 3000);
+    });
+    // Mouseup eventListener set to clear holdTimer on mouse up
+    thumbScanner.addEventListener("mouseup", () => {
+        clearTimeout(holdTimer);
+    });
+
+    // Mouseleave eventListener set to clear holdTimer if mouse leaves scanner
+    thumbScanner.addEventListener("mouseleave", () => {
+        clearTimeout(holdTimer);
+    });
     
     confirmNotif.append(exclamation, confirmCaption);
     printScanner.append(thumbScanner);
